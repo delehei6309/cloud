266b024dabccc5bb0b1e6e647db473f92daee2f3
qimeiyan <qmy20140422@163.com>
2017-07-10 13:50:25 +0800

    change name

--- src/containers/ProductDetail.vue
+++ src/containers/ProductDetail.vue
@@ -1,14 +1,176 @@
 <template>
-    <div class="product">
-        product
-    </div>
-</template>
+        <div class="fixi-goods-detail">
+            <div class="title1">定期产品详情</div>
+            <div class="content">
+                <h2>基本要素</h2>
+                <div class="main" flex="dir:left main:justify box:mean">
+                    <ul flex-box="1">
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">产品名称</span>
+                            <span class="info" flex-box="1">00001</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">产品类型</span>
+                            <span class="info" flex-box="1">184****7676</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">收益方式</span>
+                            <span class="info" flex-box="1">小细腿</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">可转债权</span>
+                            <span class="info" flex-box="1">小细腿</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">投资人条件</span>
+                            <span class="info" flex-box="1">小细腿</span>
+                        </li>
+                    </ul>
+                    <ul  flex-box="1">
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">产品规模</span>
+                            <span class="info" flex-box="1">184****7676</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">产品期限</span>
+                            <span class="info" flex-box="1">小细腿</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">预期年化收益率</span>
+                            <span class="info" flex-box="1">小细腿</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">起投金额</span>
+                            <span class="info" flex-box="1">小细腿</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">投资上限</span>
+                            <span class="info" flex-box="1">00001</span>
+                        </li>
+                    </ul>
+                    <ul  flex-box="1"class="right">
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">募集金额(实)</span>
+                            <span class="info" flex-box="1">00001</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">募集金额(虚)</span>
+                            <span class="info" flex-box="1">184****7676</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">产品状态</span>
+                            <span class="info" flex-box="1">小细腿</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="content content2">
+                <h2>项目周期</h2>
+                <div class="main" flex="dir:left">
+                    <ul>
+                        <li>发布日：2017-03-10</li>
+                        <li>预期收款日：2017-03-10</li>
+                    </ul>
+                    <ul>
+                        <li>起息日：2017-03-10</li>
+                        <li>成立日：2017-03-10</li>
+                    </ul>
+                    <ul>
+                        <li>到期日：2017-03-10</li>
+                    </ul>
+                </div>
 
+            </div>
+            <div class="content content3">
+                <h2>募集方</h2>
+                <div class="main" >
+                    <ul >
+                        <li flex="box:mean ">
+                            <span class="title" flex-box="1">企业用户编号：</span>
+                            <span class="info" flex-box="6">184****100000675</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1" >企业名称：</span>
+                            <span class="info" flex-box="6">南京****交易所</span>
+                        </li>
+                    </ul>
+                    <div class="tab">
+                        <ul flex="dir:left">
+                            <li>项目概况</li>
+                            <li>资金投向</li>
+                            <li>资金来源</li>
+                            <li>风险保障</li>
+                            <li style="border-right:1px solid #4F97EB">风险提示</li>
+                        </ul>
+                        <p><span>委托人预期年化收益率：</span><span> 5.15%，为定向委托投资标的到期产生的预期年化收益率扣除受托投资管理机构的管理费率。</span></p>
+                        <p><span>收益起始日： </span><span>2017年03月24日</span></p>
+                        <p><span>收益到期日： </span><span>2017年07月03日</span></p>
+                        <p><span>期限：  </span><span>101天</span></p>
+                        <p><span>预期收款日： </span><span>到期日后三个工作日内（以实际收款日为准）</span></p>
+                        <p><span>定向委托投资标的： </span><span>非银行金融机构发行的产品，详见《定向委托投资标的说明》</span></p>
+                        <p><span>定向委托投资标的风险提示： </span><span> 详见《风险提示函》</span></p>
+                        <p><span>受托投资管理机构： </span><span>  深圳平安聚鑫资产管理有限公司</span></p>
+                        <p><span>托管机构： </span><span>  平安银行股份有限公司深圳分行</span></p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="content content4">
+                <h2>产品附件</h2>
+                <div class="main">
+                    <ul>
+                        <li>托管机构： 平安银行股份有限公司深圳分行.pdf</li>
+                        <li>托管机构： 平安银行股份有限公司深圳分行.pdf</li>
+                        <li>托管机构： 平安银行股份有限公司深圳分行.pdf</li>
+                        <li>托管机构： 平安银行股份有限公司深圳分行.pdf</li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="content content5">
+                <h2>状态</h2>
+                <div class="main" flex="dir:left main:justify box:mean">
+                    <ul  flex-box="1">
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">上架状态：</span>
+                            <span class="info" flex-box="1">已上架</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">产品状态：</span>
+                            <span class="info" flex-box="1">已成立</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">推荐状态：</span>
+                            <span class="info" flex-box="1">未推荐</span>
+                        </li>
+                    </ul>
+                    <ul  flex-box="1">
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">置顶状态：</span>
+                            <span class="info" flex-box="1">未置顶</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">放款状态：</span>
+                            <span class="info" flex-box="1">已放款</span>
+                        </li>
+                        <li flex="box:mean">
+                            <span class="title" flex-box="1">还款状态：</span>
+                            <span class="info" flex-box="1">未申请</span>
+                        </li>
+                    </ul>
+                    <ul flex-box="1"></ul>
+                </div>
+            </div>
+        </div>
+</template>
 <script>
+    import '../less/fixi-goods-detail.less';
     export default {
-        name: 'product',
+        name: 'fixi-goods-detail',
         data(){
-            return {}
+            return {
+                id:'131181199103081929'
+            }
         },
         created(){
         },
