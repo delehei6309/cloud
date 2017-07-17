a12915e9bc4877f5922f13438bdf1e4406f2aeac
delehei <wangchunting7@163.com>
2017-07-17 14:41:28 +0800

    config

--- src/tools/config.js
+++ src/tools/config.js
@@ -2,8 +2,10 @@
  * Created by DELL on 2017/6/5.
  */
 let protocol = window.location.protocol;
-export const devUrl = 'http://10.10.10.98:8079';
-export const testUrl = `${protocol}//weixin-test.zj-hf.cn`;
+//export const devUrl = 'http://10.10.10.98:8079';
+export const devUrl = 'http://47.93.34.197:8079';
+//export const testUrl = `${protocol}//weixin-test.zj-hf.cn`;
+export const testUrl = `${protocol}//47.93.34.197:8079`;
 export const productionUrl = `${protocol}//zj-weixin.zj-hf.cn`;
 /*export const baofooCallUrlProduct = `${protocol}//callme.zj-hf.cn`;
 export const baofooCallUrlTest = testUrl;*/
