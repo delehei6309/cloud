683634ef1b93221c6d234cd27603dc6cbfd55551
二三子 <wangchunting7@163.com>
2017-08-15 17:45:21 +0800

    develop

--- src/containers/ExchangeManagement.vue
+++ src/containers/ExchangeManagement.vue
@@ -89,7 +89,7 @@
                 <span class="span-close" @click.stop="lookOverShow = false">×</span>
                 <h6>查看协议</h6>
                 <div class="content" flex="dir:top  box:mean" flex-box="1">
-                    <div flex="dir:top main:center"><a :href="agreementSrc" target="_blank">《认购协议》</a></div>
+                    <div flex="dir:top main:center"><a @click.stop="postPdf()">《认购协议》</a></div>
                     <!-- <div flex="dir:top main:center"><a href="">《转让协议》</a></div> -->
                 </div>
                 <div class="box-bottom"><b-btn class="btn" @click.stop="lookOverShow = false">关闭</b-btn></div>
@@ -170,6 +170,7 @@
                 },
                 currentPage: 1,
                 perPage: 10,
+                canClick:true
             }
         },
         components: { datepicker },
@@ -192,15 +193,23 @@
         },
         methods: {
             lookOver(src){
-                let fileName = '认购协议';
+                this.agreementSrc = src;
+                this.lookOverShow = true;
+            },
+            postPdf(){
+                if(!this.canClick){
+                    return false;
+                }
+                this.canClick = false;
                 $api.post('/trade/getAccessFileUrl',{
-                    fileName:fileName
+                    fileName:this.agreementSrc
                 }).then(msg => {
                     if(msg.code == 200){
-                        this.agreementSrc = msg.data;
-                        this.lookOverShow = true;
+                        this.canClick = true;
+                        window.open(msg.data,'协议');
                     }else{
                         Toast(msg.msg);
+                        this.canClick = true;
                     }
                 });
             },
